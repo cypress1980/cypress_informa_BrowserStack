581b5342b45ab0dd39998a4c8c24479f0d4a2a52
informa1980 <you@example.com>
2024-07-17 13:31:08 +0530

    added feature file

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,9 @@
 const { defineConfig } = require("cypress");
-const cucumber = require("cypress-cucumber-preprocessor").default;
+
 module.exports = defineConfig({
   e2e: {
-    specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
-      on("file:preprocessor", cucumber());
+      // implement node event listeners here
     },
   },
 });
